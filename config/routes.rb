f5a62a1a9a88d760e29c97e88da9eb33ced6aca7
ivanderlich@gmail.com <ivanderlich@gmail.com>
2019-12-06 18:28:04 -0300

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root 'events#index'
+
   # User
   resources :users, only: [:new, :create, :show]
 
   # Sessions
   get    '/login',   to: 'sessions#new'
   resources :sessions, only: [:create, :destroy]
+
+  # Events
+  resources :events, only: [:new, :create, :index]
 end

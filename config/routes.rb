7c37764032a3ab4a7de01a4a11808b8e22d22d8d
ivanderlich@gmail.com <ivanderlich@gmail.com>
2019-12-06 17:42:29 -0300

    Edit login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # User
   resources :users, only: [:new, :create, :show]
-  resources :sessions, only: [:new, :create, :destroy]
+
+  # Sessions
+  get    '/login',   to: 'sessions#new'
+  resources :sessions, only: [:create, :destroy]
 end

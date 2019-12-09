925e236bc37649f9e1e16a141075ebc025dbf3fd
ViriCruz <viridiana.cruz.olivera@gmail.com>
2019-12-09 15:31:48 -0700

    Modify foreign_key for events table

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema.define(version: 2019_12_06_164339) do
   create_table "events", force: :cascade do |t|
     t.string "description"
     t.date "date"
+    t.integer "creator_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +28,5 @@ ActiveRecord::Schema.define(version: 2019_12_06_164339) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end

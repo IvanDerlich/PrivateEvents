64e42946c69621ae69605a13e2484782cb312404
ivanderlich@gmail.com <ivanderlich@gmail.com>
2019-12-09 00:19:52 -0300

    Add test for models: user and event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,6 +1,16 @@
 RSpec.describe 'Event', type: :feature do
-    xit '#invalid event: empty description' do        
+
+    fixtures :users, :events
+
+    before :each do
+        @user1 = users(:ivan)        
+        @user2 = User.new(name: 'Pablo', email: 'pablo@example.com')
     end
+
+    xit '# create invalid event: empty description' do  
+        event = Event.new(creator: user, description: '', date: '')
+        expect(event).to_not be_valid          
+    end    
 
     xit '#invalid event: empty date' do        
     end    
@@ -11,6 +21,13 @@ RSpec.describe 'Event', type: :feature do
     xit '#invalid event: invalid date' do        
     end
 
-    xit '#valid event' do        
+    it '# creates a valid event' do    
+        event = Event.new(creator: @user1, description: 'testing event', date: '')    
+        expect(event).to be_valid   
+    end
+    
+    it '# creates valid events from fixtures' do    
+        expect(events(:event1) ).to be_valid   
+        expect(events(:event2) ).to be_valid 
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'User', type: :feature do
+    fixtures :users
 
     xit '# invalid user: empty name' do        
         user = User.new(name: '', email: 'pablo@example.com')
@@ -24,9 +25,10 @@ RSpec.describe 'User', type: :feature do
         expect(user).to_not be_valid       
     end    
 
-    it '# accepts a valid user' do        
-        user = User.new(name: 'Pablo', email: 'pablo@example.com')
-        expect(user).to be_valid
-    end
+    it '# creates valid users from fixtures' do 
+        expect(users(:pablo)).to be_valid
+        expect(users(:ivan)).to be_valid
+        expect(users(:gabriela)).to be_valid
+    end   
 
 end

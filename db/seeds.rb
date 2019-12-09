9aa1f22582afa430f3813ec2546a3b02787ca098
ivanderlich@gmail.com <ivanderlich@gmail.com>
2019-12-09 00:16:17 -0300

    Seed 51 users and 50 events recursively

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,32 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-user = User.new(name: "Pablo", email: "pablo@example.com")
-user.save
+user = User.create!(
+  name: "Pablo", 
+  email: "pablo@example.com"
+)
+
 Event.create!(
   creator: user, 
   description: "Pepe's birthday",
   date: "2019-22-08 20:00:00"
 )
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  User.create!(
+    name:  name,
+    email: email   
+    )
+end
+
+users = User.order(:created_at).take(6)
+
+50.times do
+  description = Faker::Superhero.name+"'s promotion to "+Faker::Military.air_force_rank
+  users.each { |user| 
+      event = Event.new(creator: user, description: description, date: Time.now)
+      #user.events.create!(description: description) -> How to do it this way?
+  }
+end
+

d2e0a1f87745e32cfbdd6a4574d8fbf1e0348609
ivanderlich@gmail.com <ivanderlich@gmail.com>
2019-12-09 00:17:08 -0300

    Add validation suggestions in comments for future work

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
+    #validates :description,  presence: true, length: { maximum: 50 }    
+    #description with minimun and maximum size
+    #validate that date makes sense and is not just a string
+
     belongs_to :creator, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+    #before_save :downcase_email
+
+    #validates :name,  presence: true, length: { maximum: 50 }    
+    #name with minimun and maximum size   
+
+    #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    # validates :email, presence: true, length: { maximum: 255 }, 
+    #     format: { with: VALID_EMAIL_REGEX },
+    #     uniqueness: { case_sensitive: false }
+
     has_many :events
 end

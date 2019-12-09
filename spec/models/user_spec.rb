a915a6faf902c7c28e8a11c0e7a2eee00da93918
ivanderlich@gmail.com <ivanderlich@gmail.com>
2019-12-08 21:25:09 -0300

    Add tests to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
 RSpec.describe 'User', type: :feature do
-    xit '#invalid user: empty name' do        
+
+    xit '# invalid user: empty name' do        
+        user = User.new(name: '', email: 'pablo@example.com')
+        expect(user).to_not be_valid       
     end
 
-    xit '#invalid user: empty email' do        
+    xit '# invalid user: empty email' do   
+        user = User.new(name: 'Pablo', email: '')
+        expect(user).to_not be_valid            
     end    
 
-    xit '#invalid user: invalid name' do        
+    xit '# invalid user: invalid name' do  
+        user = User.new(name: 'P', email: 'pablo@example.com')
+        expect(user).to_not be_valid       
     end
 
-    xit '#invalid user: invalid email' do        
+    xit '# rejects aninvalid user: invalid email' do 
+        user = User.new(name: 'Pablo', email: 'pablo@example')
+        expect(user).to_not be_valid       
     end    
 
-    xit '#valid user' do        
+    it '# accepts a valid user' do        
+        user = User.new(name: 'Pablo', email: 'pablo@example.com')
+        expect(user).to be_valid
     end
+
 end

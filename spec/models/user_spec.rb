fb6d66954ee29b3aa70a247ed070d5d8f8ab5563
ivanderlich@gmail.com <ivanderlich@gmail.com>
2019-12-09 14:24:13 -0300

    Minor changes in user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,22 +5,22 @@ require 'rails_helper'
 RSpec.describe 'User', type: :feature do
     fixtures :users
 
-    xit '# invalid user: empty name' do        
+    it '# invalid user: empty name' do        
         user = User.new(name: '', email: 'pablo@example.com')
         expect(user).to_not be_valid       
     end
 
-    xit '# invalid user: empty email' do   
+    it '# invalid user: empty email' do   
         user = User.new(name: 'Pablo', email: '')
         expect(user).to_not be_valid            
     end    
 
-    xit '# invalid user: invalid name' do  
+    it '# invalid user: invalid name' do  
         user = User.new(name: 'P', email: 'pablo@example.com')
         expect(user).to_not be_valid       
     end
 
-    xit '# rejects aninvalid user: invalid email' do 
+    it '# rejects aninvalid user: invalid email' do 
         user = User.new(name: 'Pablo', email: 'pablo@example')
         expect(user).to_not be_valid       
     end    

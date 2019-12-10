c4eb4ab2a698e9c29079bf7840de5ff511f129ce
ViriCruz <viridiana.cruz.olivera@gmail.com>
2019-12-10 12:26:56 -0700

    Add logic to new, create, index and show methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,29 @@
+require 'time'
 class EventsController < ApplicationController
   def new
+    @event = Event.new
   end
 
   def create
+    @event = current_user.events.build(event_params)
+    @event.date = Time.now.to_s
+    if @event.save
+      redirect_to current_user
+    else
+      render 'new'
+    end
   end
 
   def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find_by(id: params[:id])
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:description)
   end
 end
